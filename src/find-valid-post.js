f84f99a2b9b7574e2193ccdb2fce445303f618dc
Paul Richard <heythisispaul@gmail.com>
2019-01-08 22:16:19 -0700

    accounts for appended url when checking past tweets

--- src/find-valid-post.js
+++ src/find-valid-post.js
@@ -1,9 +1,15 @@
+export const tweetMatch = (tweets, value) => {
+    return value.length > 100 ? 
+    tweets.filter((tweet) => tweet.includes(value.substring(0, 99))).length < 1 : 
+    !tweets.includes(value);
+}
+
 export default (posts, tweets) => {
     const tweetLength = 240;
     let post = null;
     for (let i = 0; i < posts.length; i++) {
         let tweetText = posts[i].data.title;
-        if (tweetText.length <= tweetLength && tweets.indexOf(tweetText) < 0) {
+        if (tweetText.length <= tweetLength && tweetMatch(tweets, tweetText)) {
             post = posts[i].data.title;
             break;
         }
